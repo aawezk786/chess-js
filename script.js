6a01034c7d543773d89aa4969b3cd0628750d0b1
aawezk456 <aawezk456@gmail.com>
2020-10-26 11:05:16 +0530

    Structure and css Added

---
+++ script.js
@@ -0,0 +1,240 @@
+let main = {
+
+  variables: {
+    turn: 'w',
+    selectedpiece: '',
+    highlighted: [],
+    pieces: {
+      w_king: {
+        position: '5_1',
+        img: '&#9812;',
+        captured: false,
+        moved: false,
+        type: 'w_king'
+        
+      },
+      w_queen: {
+        position: '4_1',
+        img: '&#9813;',
+        captured: false,
+        moved: false,
+        type: 'w_queen'
+      },
+      w_bishop1: {
+        position: '3_1',
+        img: '&#9815;',
+        captured: false,
+        moved: false,
+        type: 'w_bishop'
+      },
+      w_bishop2: {
+        position: '6_1',
+        img: '&#9815;',
+        captured: false,
+        moved: false,
+        type: 'w_bishop'
+      },
+      w_knight1: {
+        position: '2_1',
+        img: '&#9816;',
+        captured: false,
+        moved: false,
+        type: 'w_knight'
+      },
+      w_knight2: {
+        position: '7_1',
+        img: '&#9816;',
+        captured: false,
+        moved: false,
+        type: 'w_knight'
+      },
+      w_rook1: {
+        position: '1_1',
+        img: '&#9814;',
+        captured: false,
+        moved: false,
+        type: 'w_rook'
+      },
+      w_rook2: {
+        position: '8_1',
+        img: '&#9814;',
+        captured: false,
+        moved: false,
+        type: 'w_rook'
+      },
+      w_pawn1: {
+        position: '1_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn2: {
+        position: '2_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn3: {
+        position: '3_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn4: {
+        position: '4_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn5: {
+        position: '5_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn6: {
+        position: '6_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn7: {
+        position: '7_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+      w_pawn8: {
+        position: '8_2',
+        img: '&#9817;',
+        captured: false,
+        type: 'w_pawn',
+        moved: false
+      },
+
+      b_king: {
+        position: '5_8',
+        img: '&#9818;',
+        captured: false,
+        moved: false,
+        type: 'b_king'
+      },
+      b_queen: {
+        position: '4_8',
+        img: '&#9819;',
+        captured: false,
+        moved: false,
+        type: 'b_queen'
+      },
+      b_bishop1: {
+        position: '3_8',
+        img: '&#9821;',
+        captured: false,
+        moved: false,
+        type: 'b_bishop'
+      },
+      b_bishop2: {
+        position: '6_8',
+        img: '&#9821;',
+        captured: false,
+        moved: false,
+        type: 'b_bishop'
+      },
+      b_knight1: {
+        position: '2_8',
+        img: '&#9822;',
+        captured: false,
+        moved: false,
+        type: 'b_knight'
+      },
+      b_knight2: {
+        position: '7_8',
+        img: '&#9822;',
+        captured: false,
+        moved: false,
+        type: 'b_knight'
+      },
+      b_rook1: {
+        position: '1_8',
+        img: '&#9820;',
+        captured: false,
+        moved: false,
+        type: 'b_rook'
+      },
+      b_rook2: {
+        position: '8_8',
+        img: '&#9820;',
+        captured: false,
+        moved: false,
+        type: 'b_rook'
+      },
+      b_pawn1: {
+        position: '1_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn2: {
+        position: '2_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn3: {
+        position: '3_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn4: {
+        position: '4_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn5: {
+        position: '5_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn6: {
+        position: '6_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn7: {
+        position: '7_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      },
+      b_pawn8: {
+        position: '8_7',
+        img: '&#9823;',
+        captured: false,
+        type: 'b_pawn',
+        moved: false
+      }
+
+    }
+  },
+
+
+};
+
